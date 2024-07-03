97ab1ecf173faae00cd78f740dc379768d5cbbd9
(YongSun.Lee) <(yongsun.lee.8802@gmail.com)>
2024-07-03 16:06:59 +0200

    Add coronaboard-web as a regular folder

---
+++ coronaboard-web/src/components/slide.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { css } from "@emotion/react";
+import { Container } from 'react-bootstrap';
+
+export function Slide(props) {
+    const { title, children, id } = props;
+
+    return (
+        <div
+            id={id}
+            css= { css
+                `
+                    text-align: center;
+                    border-top: 1px solid #aaa;
+                    padding-top: 40px;
+                    padding-bottom: 60px;
+                    h2 {
+                        margin-bottom: 24px;
+                    }
+                `
+            }>
+            <Container>
+                <h2>{title}</h2>
+                <div>{children}</div>
+            </Container>
+        </div>
+    );
+}
